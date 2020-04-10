336c7dbbe7c4e63c260c9612644f871a50e161c6
yosuke91 <cyaojie47@gmail.com>
2020-04-10 12:05:17 +0900

    Update application_record.rb

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,3 +2,12 @@ class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 end
  
+class user < ActiveRecord::Base
+  has_many :groups_users
+  has_many :groups, through: :groups_users
+end
+
+class groups < ActiveRecord::Base
+  has_many :groups_users
+  has_many :users, through: :groups_users
+end
