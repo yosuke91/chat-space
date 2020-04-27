ad948a32e10cac18484849c66c03562a344b5c47
yosuke91 <cyaojie47@gmail.com>
2020-04-27 15:38:54 +0900

    commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ lock '3.13.0'
 set :application, 'chat-space'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:<yosuke91/chat-space.git'
+set :repo_url,  'git@github.com:yosuke91/chat-space.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
